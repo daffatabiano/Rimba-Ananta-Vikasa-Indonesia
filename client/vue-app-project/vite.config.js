daa5c6151fe42db6a09734c663f6725d7d09c85e
unknown <daffatabianoo@gmail.com>
2024-11-02 20:44:13 +0700

    fix vercel.json

--- client/vue-app-project/vite.config.js
+++ client/vue-app-project/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
