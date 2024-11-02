f6b36a8eb6787317a0f26a59580ac65ead015bef
unknown <daffatabianoo@gmail.com>
2024-11-02 21:10:35 +0700

    setup(api): set up api and envi

--- server/index.js
+++ server/index.js
@@ -19,7 +19,15 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
