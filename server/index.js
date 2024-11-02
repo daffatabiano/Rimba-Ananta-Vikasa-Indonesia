c0f12d0eec486aec4cab38f2e04c3538cda4ddd2
unknown <daffatabianoo@gmail.com>
2024-11-02 21:55:46 +0700

    fixing cors

--- server/index.js
+++ server/index.js
@@ -33,8 +33,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
