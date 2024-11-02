4bc37597085b3068109ae161f2502f244b50c4b2
unknown <daffatabianoo@gmail.com>
2024-11-02 21:37:40 +0700

    fixing cors

--- server/index.js
+++ server/index.js
@@ -20,7 +20,11 @@ db.once('open', () => {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    process.env.CLIENT_URL || 'http://localhost:5173'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
