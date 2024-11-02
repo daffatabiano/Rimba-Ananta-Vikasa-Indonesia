373407cfb5ae4bb7941cc36d888eb967f64cafbd
unknown <daffatabianoo@gmail.com>
2024-11-02 21:23:13 +0700

    fixing deployment

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ db.once('open', () => {
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 
