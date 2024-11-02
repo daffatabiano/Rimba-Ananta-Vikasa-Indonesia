77353c1b577e69e8256571f5bcc4f1b2a09e0797
unknown <daffatabianoo@gmail.com>
2024-11-02 22:01:08 +0700

    fixing cors

--- server/index.js
+++ server/index.js
@@ -27,16 +27,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
