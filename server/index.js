0a3ef4e962faf1909ad275d4b7f08dabf4e67902
unknown <daffatabianoo@gmail.com>
2024-11-02 21:58:17 +0700

    fixing cors

--- server/index.js
+++ server/index.js
@@ -19,6 +19,14 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -28,14 +36,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
