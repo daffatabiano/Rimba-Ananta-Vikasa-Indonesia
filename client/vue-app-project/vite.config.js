2818f2a40bd104e47a02f092b2da61fc570a112d
unknown <daffatabianoo@gmail.com>
2024-11-02 20:19:59 +0700

    config(vite): configiuration vite

--- client/vue-app-project/vite.config.js
+++ client/vue-app-project/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vue()],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+      __IS_PRODUCTION__: isProduction,
+    },
+    // Pilihan konfigurasi lainnya
+  };
+});
